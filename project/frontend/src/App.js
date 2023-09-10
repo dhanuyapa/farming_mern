165e53201354422aff53630bb45760eb6df142fc
dhanujaya <dhanuyapaugc@gmail.com>
2023-09-10 22:27:16 +0530

    add nav bar

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-    
+      <Header />
     </div>
   );
 }
