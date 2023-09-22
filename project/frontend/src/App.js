d798e77374d14bc7b42fbc1e25c8da17c2710412
dhanujaya <dhanuyapaugc@gmail.com>
2023-09-22 12:55:39 +0530

    frontend registration complete

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import AddCustomer from './component/AddCustomer';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<AddCustomer />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
